db6926c4dcd93f6da4daf475dff4f23b021989ca
jpb <jakub.baranski.33@gmail.com>
2024-03-13 07:16:13 +0000

    Allow saving to/fetching from database

--- Notes/Views/MainPage.xaml.cs
+++ Notes/Views/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.UI.Xaml.Controls;
 
 using Notes.ViewModels;
+using Windows.Storage;
 
 namespace Notes.Views;
 
@@ -15,5 +17,49 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    public static void AddData(string inputText)
+    {
+        string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "NotesDatabase.db");
+        using (SqliteConnection db =
+          new SqliteConnection($"Filename={dbpath}"))
+        {
+            db.Open();
+
+            SqliteCommand insertCommand = new SqliteCommand();
+            insertCommand.Connection = db;
+
+            // Use parameterized query to prevent SQL injection attacks
+            insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
+            insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+            insertCommand.ExecuteReader();
+        }
+
+    }
+
+    public static List<String> GetData()
+    {
+        List<String> entries = new List<string>();
+
+        string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "NotesDatabase.db");
+        using (SqliteConnection db =
+           new SqliteConnection($"Filename={dbpath}"))
+        {
+            db.Open();
+
+            SqliteCommand selectCommand = new SqliteCommand
+                ("SELECT Text_Entry from MyTable", db);
+
+            SqliteDataReader query = selectCommand.ExecuteReader();
+
+            while (query.Read())
+            {
+                entries.Add(query.GetString(0));
+            }
+        }
+
+        return entries;
     }
 }
