c733c39b2edf6c3b72aa854e658ed1a8bf778883
jpb <jakub.baranski.33@gmail.com>
2024-03-13 20:01:46 +0000

    Allow saving of notes into database

--- Notes/DataAccess.cs
+++ Notes/DataAccess.cs
@@ -29,5 +29,48 @@ namespace Notes
                 createTable.ExecuteReader();
             }
         }
+        public static void AddData(string inputText)
+        {
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "NotesDatabase.db");
+            using (SqliteConnection db =
+              new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO NotesTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+
+                insertCommand.ExecuteReader();
+            }
+
+        }
+
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+
+            string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "NotesDatabase.db");
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from NotesTable", db);
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+            }
+
+            return entries;
+        }
     }
 }
--- Notes/Views/MainPage.xaml.cs
+++ Notes/Views/MainPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Data.Sqlite;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 using Notes.ViewModels;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace Notes.Views;
 
@@ -17,49 +20,89 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+        NoteStorage.ItemsSource = DataAccess.GetData();
     }
 
-    public static void AddData(string inputText)
+    private async void SaveContent()
     {
-        string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "NotesDatabase.db");
-        using (SqliteConnection db =
-          new SqliteConnection($"Filename={dbpath}"))
+        DataAccess.AddData(Title.Text);
+
+        NoteStorage.ItemsSource = DataAccess.GetData();
+        byte[] bytes;
+        StorageFile file = await DownloadsFolder.CreateFileAsync("file.rtf", CreationCollisionOption.GenerateUniqueName);
+
+        if (file != null)
         {
-            db.Open();
-
-            SqliteCommand insertCommand = new SqliteCommand();
-            insertCommand.Connection = db;
-
-            // Use parameterized query to prevent SQL injection attacks
-            insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, @Entry);";
-            insertCommand.Parameters.AddWithValue("@Entry", inputText);
-
-            insertCommand.ExecuteReader();
-        }
-
-    }
-
-    public static List<String> GetData()
-    {
-        List<String> entries = new List<string>();
-
-        string dbpath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "NotesDatabase.db");
-        using (SqliteConnection db =
-           new SqliteConnection($"Filename={dbpath}"))
-        {
-            db.Open();
-
-            SqliteCommand selectCommand = new SqliteCommand
-                ("SELECT Text_Entry from MyTable", db);
-
-            SqliteDataReader query = selectCommand.ExecuteReader();
-
-            while (query.Read())
+            // write to file
+            using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
             {
-                entries.Add(query.GetString(0));
-            }
-        }
+                editor.Document.SaveToStream(Microsoft.UI.Text.TextGetOptions.FormatRtf, randAccStream);
 
-        return entries;
+                randAccStream.Seek(0);
+                var dataReader = new DataReader(randAccStream);
+                await dataReader.LoadAsync((uint)randAccStream.Size);
+                bytes = new byte[randAccStream.Size];
+                dataReader.ReadBytes(bytes);
+            }
+
+
+            SqliteConnection connection = new SqliteConnection("Filename=richEditBox.db");
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+            CREATE TABLE IF NOT EXISTS myTable (
+                content BLOB
+            )
+            ";
+            command.ExecuteNonQuery();
+
+            var insertCommand = connection.CreateCommand();
+            insertCommand.CommandText =
+            @"
+            INSERT INTO myTable (content)
+            VALUES (@content)
+            ";
+
+            insertCommand.Parameters.Add("@content", SqliteType.Blob, bytes.Length);
+            insertCommand.Parameters["@content"].Value = bytes;
+
+            var task1 = insertCommand.ExecuteNonQueryAsync();
+        }
+    }
+
+    private async void LoadContent()
+    {
+        Title.Text = NoteStorage.SelectedItem.ToString();
+
+        object[] values = new object[2];
+
+        SqliteConnection connection = new SqliteConnection("Filename=richEditBox.db");
+        connection.Open();
+        var retrieveCommand = connection.CreateCommand();
+        retrieveCommand.CommandText =
+            @"SELECT * from myTable";
+        var reader = retrieveCommand.ExecuteReader();
+        while (reader.Read())
+        {
+            System.Diagnostics.Debug.WriteLine(reader.GetValues(values));
+        }
+        var dataBytes = values[0] as byte[];
+        var dataBuffer = dataBytes.AsBuffer();
+        InMemoryRandomAccessStream randomAccessStream = new InMemoryRandomAccessStream();
+        await randomAccessStream.WriteAsync(dataBuffer);
+        randomAccessStream.Seek(0);
+        editor.Document.LoadFromStream(Microsoft.UI.Text.TextSetOptions.FormatRtf, randomAccessStream);
+    }
+
+    private void Save_Click(object sender, RoutedEventArgs e)
+    {
+        SaveContent();
+    }
+
+    private void Load_Click(object sender, RoutedEventArgs e)
+    {
+        LoadContent();
     }
 }
